912fb8ec69bdbba8c118aa8d548cfcc1e56a94a6
arshiashirzad <sirzad383@gmail.com>
2024-04-29 16:40:34 +0330

    Added and modified Header file

--- Complex.h
+++ Complex.h
@@ -1,5 +1,5 @@
-#ifndef COMPLEX_H
-#define COMPLEX_H
+#ifndef Complex_H
+#define Complex_H
 
 #include <iostream>
 #include <sstream>
@@ -11,35 +11,43 @@ private:
 
 public:
     // Default constructor
-    Complex();
+    Complex() : a(0), b(0) {}
 
     // Parameterized constructor
-    Complex(double aVal, double bVal);
-
-    // Copy constructor
-    Complex(const Complex& other);
+    Complex(double aVal, double bVal) : a(aVal), b(bVal) {}
+    //Copy cunstructor
+    Complex(const Complex& other) {
+        a = other.a;
+        b = other.b;
+    }
 
     // Getter for variable 'a'
-    double getA() const;
+    double getA() const {
+        return a;
+    }
 
     // Setter for variable 'a'
-    void setA(double value);
+    void setA(double value) {
+        a = value;
+    }
 
     // Getter for variable 'b'
-    double getB() const;
+    double getB() const {
+        return b;
+    }
 
     // Setter for variable 'b'
-    void setB(double value);
-
-    // To string method
-    string toString() const;
-
+    void setB(double value) {
+        b = value;
+    }
+    //To string method
     // Friend functions for operations
     friend Complex multiply(const Complex& obj1, const Complex& obj2);
     friend Complex divide(const Complex& obj1, const Complex& obj2);
     friend Complex add(const Complex& obj1, const Complex& obj2);
     friend Complex subtract(const Complex& obj1, const Complex& obj2);
-    friend Complex power(const Complex& obj, int exp);
+    friend Complex power(const Complex& obj , int exp);
+    string toString() const;
 };
 
 #endif
