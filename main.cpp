6f3e5b096c6ee08702648f511171e6f7740590a7
arshiashirzad <sirzad383@gmail.com>
2024-04-29 16:00:59 +0330

    Defined friend functions bodis

--- main.cpp
+++ main.cpp
@@ -44,6 +44,48 @@ public:
     friend Complex add(const Complex& obj1, const Complex& obj2);
     friend Complex subtract(const Complex& obj1, const Complex& obj2);
 };
+
+// Multiplication function
+Complex multiply(const Complex& obj1, const Complex& obj2) {
+    double new_a = obj1.a * obj2.a;
+    double new_b = obj1.b * obj2.b;
+    return Complex(new_a, new_b);
+}
+
+// Division function
+Complex divide(const Complex& obj1, const Complex& obj2) {
+    double new_a = obj1.a / obj2.a;
+    double new_b = obj1.b / obj2.b;
+    return Complex(new_a, new_b);
+}
+
+// Addition function
+Complex add(const Complex& obj1, const Complex& obj2) {
+    double new_a = obj1.a + obj2.a;
+    double new_b = obj1.b + obj2.b;
+    return Complex(new_a, new_b);
+}
+
+// Subtraction function
+Complex subtract(const Complex& obj1, const Complex& obj2) {
+    double new_a = obj1.a - obj2.a;
+    double new_b = obj1.b - obj2.b;
+    return Complex(new_a, new_b);
+}
+
 int main() {
+    Complex obj1(2, 3);
+    Complex obj2(2, 2);
+
+    Complex result_multiply = multiply(obj1, obj2);
+    Complex result_divide = divide(obj1, obj2);
+    Complex result_add = add(obj1, obj2);
+    Complex result_subtract = subtract(obj1, obj2);
+
+    cout << "Multiplication: a = " << result_multiply.getA() << ", b = " << result_multiply.getB() << endl;
+    cout << "Division: a = " << result_divide.getA() << ", b = " << result_divide.getB() << endl;
+    cout << "Addition: a = " << result_add.getA() << ", b = " << result_add.getB() << endl;
+    cout << "Subtraction: a = " << result_subtract.getA() << ", b = " << result_subtract.getB() << endl;
+
     return 0;
 }
