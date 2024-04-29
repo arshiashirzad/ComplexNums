8af81bf7536a1c3b545cbffddd44e412ef255b9d
arshiashirzad <sirzad383@gmail.com>
2024-04-29 16:41:55 +0330

    fixed header file

--- Complex.h
+++ Complex.h
@@ -1,5 +1,5 @@
-#ifndef Complex_H
-#define Complex_H
+#ifndef COMPLEX_H
+#define COMPLEX_H
 
 #include <iostream>
 #include <sstream>
@@ -11,43 +11,35 @@ private:
 
 public:
     // Default constructor
-    Complex() : a(0), b(0) {}
+    Complex();
 
     // Parameterized constructor
-    Complex(double aVal, double bVal) : a(aVal), b(bVal) {}
-    //Copy cunstructor
-    Complex(const Complex& other) {
-        a = other.a;
-        b = other.b;
-    }
+    Complex(double aVal, double bVal);
+
+    // Copy constructor
+    Complex(const Complex& other);
 
     // Getter for variable 'a'
-    double getA() const {
-        return a;
-    }
+    double getA() const;
 
     // Setter for variable 'a'
-    void setA(double value) {
-        a = value;
-    }
+    void setA(double value);
 
     // Getter for variable 'b'
-    double getB() const {
-        return b;
-    }
+    double getB() const;
 
     // Setter for variable 'b'
-    void setB(double value) {
-        b = value;
-    }
-    //To string method
+    void setB(double value);
+
+    // To string method
+    string toString() const;
+
     // Friend functions for operations
     friend Complex multiply(const Complex& obj1, const Complex& obj2);
     friend Complex divide(const Complex& obj1, const Complex& obj2);
     friend Complex add(const Complex& obj1, const Complex& obj2);
     friend Complex subtract(const Complex& obj1, const Complex& obj2);
-    friend Complex power(const Complex& obj , int exp);
-    string toString() const;
+    friend Complex power(const Complex& obj, int exp);
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
-#ifndef COMPLEX_H
-#define COMPLEX_H
-
 #include <iostream>
 #include <sstream>
 using namespace std;
+
 class Complex {
 private:
     double a;
@@ -11,35 +9,103 @@ private:
 
 public:
     // Default constructor
-    Complex();
+    Complex() : a(0), b(0) {}
 
     // Parameterized constructor
-    Complex(double aVal, double bVal);
-
-    // Copy constructor
-    Complex(const Complex& other);
+    Complex(double aVal, double bVal) : a(aVal), b(bVal) {}
+    //Copy cunstructor
+    Complex(const Complex& other) {
+        a = other.a;
+        b = other.b;
+    }
 
     // Getter for variable 'a'
-    double getA() const;
+    double getA() const {
+        return a;
+    }
 
     // Setter for variable 'a'
-    void setA(double value);
+    void setA(double value) {
+        a = value;
+    }
 
     // Getter for variable 'b'
-    double getB() const;
+    double getB() const {
+        return b;
+    }
 
     // Setter for variable 'b'
-    void setB(double value);
-
-    // To string method
-    string toString() const;
-
+    void setB(double value) {
+        b = value;
+    }
+    //To string method
+    string toString() const {
+        stringstream ss;
+        ss << a<< "+" << b <<"i";
+        return ss.str();
+    }
     // Friend functions for operations
     friend Complex multiply(const Complex& obj1, const Complex& obj2);
     friend Complex divide(const Complex& obj1, const Complex& obj2);
     friend Complex add(const Complex& obj1, const Complex& obj2);
     friend Complex subtract(const Complex& obj1, const Complex& obj2);
-    friend Complex power(const Complex& obj, int exp);
+    friend Complex power(const Complex& obj , int exp);
 };
 
-#endif
+// Multiplication function
+Complex multiply(const Complex& obj1, const Complex& obj2) {
+    double new_a = obj1.a * obj2.a;
+    double new_b = obj1.b * obj2.b;
+    return Complex(new_a, new_b);
+}
+
+// Division function
+Complex divide(const Complex& obj1, const Complex& obj2) {
+    double new_a = obj1.a / obj2.a;
+    double new_b = obj1.b / obj2.b;
+    return Complex(new_a, new_b);
+}
+
+// Addition function
+Complex add(const Complex& obj1, const Complex& obj2) {
+    double new_a = obj1.a + obj2.a;
+    double new_b = obj1.b + obj2.b;
+    return Complex(new_a, new_b);
+}
+
+// Subtraction function
+Complex subtract(const Complex& obj1, const Complex& obj2) {
+    double new_a = obj1.a - obj2.a;
+    double new_b = obj1.b - obj2.b;
+    return Complex(new_a, new_b);
+}
+Complex power(const Complex& obj, int exp){
+    double new_a = obj.a * obj.a;
+    double new_b= obj.b * obj.b;
+    for(int i=1 ; i<exp ;i++ ){
+        new_a *= obj.a;
+        new_a *= obj.b;
+    }
+    return Complex(new_a , new_b);
+}
+
+int main() {
+    Complex obj1(2, 3);
+    Complex obj2(2, 2);
+
+    Complex resMultiply = multiply(obj1, obj2);
+    Complex resDivide = divide(obj1, obj2);
+    Complex resAdd = add(obj1, obj2);
+    Complex resSub = subtract(obj1, obj2);
+    Complex resPowA = power(obj1, 3);
+    Complex resPowB = power(obj2, 3);
+
+    cout << "Multiplication:" << resMultiply.toString() << endl;
+    cout << "Division: a = " << resDivide.toString() << endl;
+    cout << "Addition: a = " << resAdd.toString() << endl;
+    cout << "Subtraction: a = " << resSub.toString() << endl;
+    cout << "Power of a = "<< resPowA.toString()<<endl;
+    cout << "Power of b = "<< resPowB.toString()<<endl;
+
+    return 0;
+}
